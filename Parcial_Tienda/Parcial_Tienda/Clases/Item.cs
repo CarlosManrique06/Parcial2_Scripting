66eddb8507ce4c9ed51d47753a098e1e7ac30ad2
Granada0555 <114270479+Granada0555@users.noreply.github.com>
2026-03-16 21:10:15 -0500

    Rename ItemCategory to ItemCategoria in Item class

--- Parcial_Tienda/Parcial_Tienda/Clases/Item.cs
+++ Parcial_Tienda/Parcial_Tienda/Clases/Item.cs
@@ -8,11 +8,12 @@ namespace Parcial_Tienda.Clases
 
 public class Item
 {
+
     public string Name { get; }
     public decimal Price { get; }
-    public Itemcategory Category { get; }
+    public ItemCategoria Category { get; }
 
-    public Item(string name, decimal price, ItemCategory category)
+    public Item(string name, decimal price, ItemCategoria category)
     {
         if (string.IsNullOrWhiteSpace(name))
             throw new ArgumentException("Nombre inválido");
@@ -24,4 +25,5 @@ public class Item
         Price = price;
         Category = category;
     }
+
 }
