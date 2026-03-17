cb67ca454e06af919e89ef8dd1abbb26a0af60a8
CarlosManrique06 <cajumalo06@gmail.com>
2026-03-16 21:13:30 -0500

    Cambio_Mini_corchete

--- Parcial_Tienda/Parcial_Tienda/Clases/Item.cs
+++ Parcial_Tienda/Parcial_Tienda/Clases/Item.cs
@@ -5,25 +5,27 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Parcial_Tienda.Clases
-
-public class Item
 {
 
-    public string Name { get; }
-    public decimal Price { get; }
-    public ItemCategoria Category { get; }
-
-    public Item(string name, decimal price, ItemCategoria category)
+    public class Item
     {
-        if (string.IsNullOrWhiteSpace(name))
-            throw new ArgumentException("Nombre inválido");
 
-        if (price <= 0)
-            throw new ArgumentException("El precio debe ser positivo");
+        public string Name { get; }
+        public decimal Price { get; }
+        public ItemCategoria Category { get; }
 
-        Name = name;
-        Price = price;
-        Category = category;
+        public Item(string name, decimal price, ItemCategoria category)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Nombre inválido");
+
+            if (price <= 0)
+                throw new ArgumentException("El precio debe ser positivo");
+
+            Name = name;
+            Price = price;
+            Category = category;
+        }
+
     }
-
 }
